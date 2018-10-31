ae8b8ae7b7f6cecff42280b4b6c50acaa27abd7c
moffatmore <mofenyimoffat@gmail.com>
2018-10-31 12:23:30 +0200

    fixing maternal eligibility listboard form and wrapper

--- td_dashboard/model_wrappers/maternal_consent_model_wrapper.py
+++ td_dashboard/model_wrappers/maternal_consent_model_wrapper.py
@@ -6,7 +6,8 @@ from edc_model_wrapper import ModelWrapper
 class MaternalConsentModelWrapper(ModelWrapper):
 
     model = 'td_maternal.subjectconsent'
-    next_url_name = settings.DASHBOARD_URL_NAMES.get('subject_dashboard_url')
+    next_url_name = settings.DASHBOARD_URL_NAMES.get(
+        'maternal_subject_dashboard_url')
     next_url_attrs = ['subject_identifier']
     querystring_attrs = [
         'eligibility_id', 'gender', 'first_name', 'initials', 'modified']
