cf568c392acbd36fe431a254c8011fb95a2922fa
moffatmore <mofenyimoffat@gmail.com>
2018-10-31 11:22:42 +0200

    fixing maternal eligibility listboard

--- td_dashboard/model_wrappers/maternal_consent_model_wrapper.py
+++ td_dashboard/model_wrappers/maternal_consent_model_wrapper.py
@@ -5,7 +5,7 @@ from edc_model_wrapper import ModelWrapper
 
 class MaternalConsentModelWrapper(ModelWrapper):
 
-    model = 'td_maternal.maternalconsent'
+    model = 'td_maternal.subjectconsent'
     next_url_name = settings.DASHBOARD_URL_NAMES.get('subject_dashboard_url')
     next_url_attrs = ['subject_identifier']
     querystring_attrs = [
---
+++ td_dashboard/templatetags/td_dashboard_extras.py
@@ -0,0 +1,46 @@
+from django import template
+from django.conf import settings
+from edc_constants.constants import ABNORMAL
+
+register = template.Library()
+
+
+@register.inclusion_tag('td_dashboard/buttons/screening_button.html')
+def screening_button(model_wrapper):
+    title = ['Edit subject\' screening form.']
+    return dict(
+        screening_identifier=model_wrapper.object.eligibility_id,
+        href=model_wrapper.href,
+        title=' '.join(title))
+
+
+@register.inclusion_tag('td_dashboard/buttons/eligibility_button.html')
+def eligibility_button(subject_screening_model_wrapper):
+    comment = []
+    obj = subject_screening_model_wrapper.object
+    tooltip = None
+    if not obj.is_eligible:
+        comment = obj.reasons_ineligible.split(',')
+    comment = list(set(comment))
+    comment.sort()
+    return dict(eligible=obj.is_eligible, comment=comment, tooltip=tooltip)
+
+
+@register.inclusion_tag('td_dashboard/buttons/consent_button.html')
+def consent_button(model_wrapper):
+    title = ['Consent subject to participate.']
+    consent_version = model_wrapper.consent.version
+    return dict(
+        screening_identifier=model_wrapper.object.eligibility_id,
+        add_consent_href=model_wrapper.consent.href,
+        consent_version=consent_version,
+        title=' '.join(title))
+
+
+@register.inclusion_tag('td_dashboard/buttons/dashboard_button.html')
+def dashboard_button(model_wrapper):
+    subject_dashboard_url = settings.DASHBOARD_URL_NAMES.get(
+        'subject_dashboard_url')
+    return dict(
+        subject_dashboard_url=subject_dashboard_url,
+        subject_identifier=model_wrapper.eligibility_id)
