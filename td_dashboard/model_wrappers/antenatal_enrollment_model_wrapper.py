a2b3df491dfd4a5f72ee55d8b3083668124f4d44
tumiemosweu <imosweuk@gmail.com>
2018-12-13 09:33:22 +0200

    fix enrollment redirect

--- td_dashboard/model_wrappers/antenatal_enrollment_model_wrapper.py
+++ td_dashboard/model_wrappers/antenatal_enrollment_model_wrapper.py
@@ -7,6 +7,6 @@ class AntenatalEnrollmentModelWrapper(ModelWrapper):
 
     model = 'td_maternal.antenatalenrollment'
     next_url_name = settings.DASHBOARD_URL_NAMES.get(
-        'subject_dashboard_url')
-    next_url_attrs = ['subject_identifier']
+        'screening_listboard_url')
+    next_url_attrs = ['screening_identifier']
     querystring_attrs = ['subject_identifier']
