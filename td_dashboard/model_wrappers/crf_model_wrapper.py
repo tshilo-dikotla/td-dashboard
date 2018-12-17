7e1dbcabe34cd1f5a2bbeee7ba564ca48cac9630
Coulson Kgathi <ckgathi@gmail.com>
2018-12-17 19:20:04 +0200

    overide crf model wrapper

--- td_dashboard/model_wrappers/crf_model_wrapper.py
+++ td_dashboard/model_wrappers/crf_model_wrapper.py
@@ -5,6 +5,8 @@ class CrfModelWrapper(BaseCrfModelWrapper):
 
     visit_model_attr = 'maternal_visit'
 
+    querystring_attrs = [visit_model_attr]
+
     @property
     def maternal_visit(self):
         return str(getattr(self.object, self.visit_model_attr).id)
