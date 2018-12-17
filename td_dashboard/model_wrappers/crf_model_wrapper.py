c6f56e168a21066b7284481345b65105281f5262
Coulson Kgathi <ckgathi@gmail.com>
2018-12-17 19:13:05 +0200

    overide crf model wrapper

--- td_dashboard/model_wrappers/crf_model_wrapper.py
+++ td_dashboard/model_wrappers/crf_model_wrapper.py
@@ -1,24 +1,10 @@
-from edc_model_wrapper import ModelWrapper
-from django.conf import settings
+from edc_visit_schedule.model_wrappers import CrfModelWrapper as BaseCrfModelWrapper
 
 
-class CrfModelWrapper(ModelWrapper):
+class CrfModelWrapper(BaseCrfModelWrapper):
 
     visit_model_attr = 'maternal_visit'
-
-    next_url_name = settings.DASHBOARD_URL_NAMES.get(
-        'subject_dashboard_url')
-    next_url_attrs = ['appointment', 'subject_identifier']
-    querystring_attrs = [visit_model_attr]
 
     @property
     def maternal_visit(self):
         return str(getattr(self.object, self.visit_model_attr).id)
-
-    @property
-    def appointment(self):
-        return str(getattr(self.object, self.visit_model_attr).appointment.id)
-
-    @property
-    def subject_identifier(self):
-        return getattr(self.object, self.visit_model_attr).subject_identifier
