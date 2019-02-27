49ff0ecfac5276646459b3d85ad66fa0d15e1969
tumiemosweu <imosweuk@gmail.com>
2019-02-27 09:40:57 +0200

    fix locator view

--- td_dashboard/model_wrappers/subject_locator_model_wrapper.py
+++ td_dashboard/model_wrappers/subject_locator_model_wrapper.py
@@ -4,6 +4,6 @@ from edc_model_wrapper.wrappers import ModelWrapper
 
 class SubjectLocatorModelWrapper(ModelWrapper):
 
-    model = 'edc_locator.subjectlocator'
+    model = 'td_maternal.maternallocator'
     next_url_name = settings.DASHBOARD_URL_NAMES.get('subject_dashboard_url')
     next_url_attrs = ['subject_identifier']
--- td_dashboard/views/maternal_subject/dashboard/dashboard_view.py
+++ td_dashboard/views/maternal_subject/dashboard/dashboard_view.py
@@ -1,16 +1,19 @@
 from django.apps import apps as django_apps
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ValidationError
+from edc_action_item.site_action_items import site_action_items
 from edc_base.view_mixins import EdcBaseViewMixin
 from edc_dashboard.views import DashboardView as BaseDashboardView
 from edc_navbar import NavbarViewMixin
-from edc_subject_dashboard.view_mixins import SubjectDashboardViewMixin
 from edc_registration.models import RegisteredSubject
+from edc_subject_dashboard.view_mixins import SubjectDashboardViewMixin
+from td_maternal.action_items import MATERNAL_LOCATOR_ACTION
 
 from ....model_wrappers import (
     AppointmentModelWrapper, SubjectConsentModelWrapper,
     MaternalVisitModelWrapper, SubjectLocatorModelWrapper,
     MaternalCrfModelWrapper, MaternalRequisitionModelWrapper,
     SubjectScreeningModelWrapper)
-from django.core.exceptions import ValidationError
 
 
 class DashboardView(
@@ -28,7 +31,7 @@ class DashboardView(
     navbar_name = 'td_dashboard'
     visit_attr = 'maternalvisit'
     navbar_selected_item = 'consented_subject'
-    subject_locator_model = 'edc_locator.subjectlocator'
+    subject_locator_model = 'td_maternal.maternallocator'
     subject_locator_model_wrapper_cls = SubjectLocatorModelWrapper
     visit_model_wrapper_cls = MaternalVisitModelWrapper
     special_forms_include_value = "td_dashboard/maternal_subject/dashboard/special_forms.html"
@@ -73,3 +76,22 @@ class DashboardView(
             existing_key=self.dashboard_url,
             context=context)
         return context
+
+    def get_subject_locator_or_message(self):
+        obj = None
+        subject_identifier = self.kwargs.get('subject_identifier')
+        try:
+            obj = self.subject_locator_model_cls.objects.get(
+                subject_identifier=subject_identifier)
+        except ObjectDoesNotExist:
+            action_cls = site_action_items.get(
+                self.subject_locator_model_cls.action_name)
+            action_item_model_cls = action_cls.action_item_model_cls()
+            try:
+                action_item_model_cls.objects.get(
+                    subject_identifier=subject_identifier,
+                    action_type__name=MATERNAL_LOCATOR_ACTION)
+            except ObjectDoesNotExist:
+                action_cls(
+                    subject_identifier=subject_identifier)
+        return obj
