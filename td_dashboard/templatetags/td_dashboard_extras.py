ac3120d0668dad0fa1126bda81c4bb324c4a836e
tumiemosweu <imosweuk@gmail.com>
2019-06-03 12:07:53 +0200

    fix version on consent button

--- td_dashboard/templatetags/td_dashboard_extras.py
+++ td_dashboard/templatetags/td_dashboard_extras.py
@@ -88,7 +88,7 @@ def consent_version_button(model_wrapper):
 @register.inclusion_tag('td_dashboard/buttons/consent_button.html')
 def consent_button(model_wrapper):
     title = ['Consent subject to participate.']
-    consent_version = model_wrapper.consent.version
+    consent_version = model_wrapper.version
     return dict(
         screening_identifier=model_wrapper.object.screening_identifier,
         add_consent_href=model_wrapper.consent.href,
